10f48ed0913c9a83669ecd1062c6f4422f1bc289
Keshav Biswa <keshavbiswa21@gmail.com>
2023-08-30 20:27:08 +0530

    some initial changes

--- src/site_config.ts
+++ src/site_config.ts
@@ -1,9 +1,9 @@
 // This file contains the site configuration for the theme.
 
 // Metadata, SEO, and Social
-export const SITE_TITLE = "Manual Theme"
-export const SITE_DESCRIPTION = "A documentation template for Astro"
-export const SITE_URL = "https://manual.otterlord.dev"
+export const SITE_TITLE = "Confuscript"
+export const SITE_DESCRIPTION = "A modern scripting language to confuse programmers."
+export const SITE_URL = "https://keshavbiswa.com/confuscript"
 export const SITE_DEFAULT_OG_IMAGE = "/assets/og-image.png"
 
 // Docs Sidebar
